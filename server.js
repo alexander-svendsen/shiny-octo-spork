935c7853583f76ff53079fffd23a87b6b3974e69
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-08-11 16:37:20 +0200

    Try to read from kafka from the beginning

--- server.js
+++ server.js
@@ -8,22 +8,24 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const client = new kafka.KafkaClient({kafkaHost: '10.166.0.5:9092'});
-const consumer = new kafka.Consumer(
-client,
-[
-    { topic: 'test', partition: 0 }
-],
-{
-    fromOffset: 'earliest',
-    autoCommit: false
-}
-);
+const consumer = new kafka.Consumer(client,[{topic: 'test', partition: 0 }],
+    {
+        autoCommit: true,
+        fetchMaxWaitMs: 1000,
+        fetchMaxBytes: 1024 * 1024,
+        encoding: 'utf8',
+        fromOffset: false
+    });
 
 
 var msg = ''
 consumer.on('message', function (message) {
     console.log(message);
     msg = message.value
+});
+
+consumer.on('error', function(err) {
+    console.log('error', err);
 });
 
 // App
