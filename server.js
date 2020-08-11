2df512768aa33b2fb03ff38765a70f49d122cc1f
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-08-11 14:46:22 +0200

    Try to add a kafka consumer

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 'use strict';
 
 const express = require('express');
+const kafka = require('kafka-node');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const client = new kafka.KafkaClient({kafkaHost: '10.166.0.5:9092'});
+const consumer = new kafka.Consumer(
+client,
+[
+    { topic: 'test', partition: 0 }
+],
+{
+    autoCommit: false
+}
+);
+
+
+var msg = ''
+consumer.on('message', function (message) {
+    console.log(message);
+    msg = message.value
+});
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello ' + msg);
 });
 
 app.listen(PORT, HOST);
